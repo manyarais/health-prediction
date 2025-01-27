6261e565c7c30326287dbe3a34403442a2c66ce1
Saurabh Raisinghani <manyaraisinghani@gmail.com>
2025-01-27 15:05:12 +0000

    all in morning Jan 27

--- src/main/java/org/springframework/samples/petclinic/patient/PatientRiskAssessment.java
+++ src/main/java/org/springframework/samples/petclinic/patient/PatientRiskAssessment.java
@@ -1,7 +1,12 @@
 package org.springframework.samples.petclinic.patient;
 
-import com.google.protobuf.Value;
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.ResponseHandler;
 import java.io.IOException;
+
+import com.google.protobuf.Value;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.HashMap;
@@ -174,10 +179,47 @@ public class PatientRiskAssessment {
 		// and get the generated recommendations
 		// This is a simplified example and needs to be replaced
 		// with your actual LLM API integration.
-		return "**Sample Recommendations:**\n" + "- Maintain a healthy diet with plenty of fruits and vegetables.\n"
-				+ "- Engage in regular physical activity, such as brisk walking or jogging.\n"
-				+ "- Manage stress through relaxation techniques like meditation or yoga.\n"
-				+ "- Schedule regular checkups with your doctor.";
+
+		String projectId = "my-genome-project-p2";
+		String location = "us-central1";
+		String modelName = "gemini-1.5-flash-001";
+		// String textPrompt =
+		// "What's a good name for a flower shop that specializes in selling bouquets of"
+		// + " dried flowers?";
+
+		String output = "";
+		try {
+			output = textInput(projectId, location, modelName, prompt);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(output);
+
+		// return "**Sample Recommendations:**\n" + "- Maintain a healthy diet with plenty
+		// of fruits and vegetables.\n"
+		// + "- Engage in regular physical activity, such as brisk walking or jogging.\n"
+		// + "- Manage stress through relaxation techniques like meditation or yoga.\n"
+		// + "- Schedule regular checkups with your doctor.";
+
+		return output;
+
+	}
+
+	// Passes the provided text input to the Gemini model and returns the text-only
+	// response.
+	// For the specified textPrompt, the model returns a list of possible store names.
+	public static String textInput(String projectId, String location, String modelName, String textPrompt)
+			throws IOException {
+		// Initialize client that will be used to send requests. This client only needs
+		// to be created once, and can be reused for multiple requests.
+		try (VertexAI vertexAI = new VertexAI(projectId, location)) {
+			GenerativeModel model = new GenerativeModel(modelName, vertexAI);
+
+			GenerateContentResponse response = model.generateContent(textPrompt);
+			String output = ResponseHandler.getText(response);
+			return output;
+		}
 	}
 
 }
